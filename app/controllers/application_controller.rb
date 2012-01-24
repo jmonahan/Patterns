df9b58428ce05c2769b6714b53f57d74fd4fac29
Joe Monahan <jmonahan167@gmail.com>
2012-01-23 19:21:48 -0800

    Removed some excess stuff, started index page with layout structure, css file structure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :init_ui_body
+  
+  def init_ui_body
+    @body_id = controller_name.classify
+    @body_class = ""
+  end
 end
